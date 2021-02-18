74c1404bd772bb6d20d6d34349ea735369d37878
Oscar Alejandro Lopez Ramos <machochinnio@hotmail.com>
2021-02-17 18:20:24 -0600

    Actividad 3 sample

---
+++ Actividad 3/Actividad3_OALR.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+/*:
+# Playground - Actividad 3
+* Tipos de datos
+* Asociación de tipos
+* Arreglos y Diccionarios
+*/
+
+/*: 
+### Actividad de Tipos de datos
+A) Declarar cuatro variables con tres diferentes tipos de datos, cada uno que corresponda a una numero entero, un numero decimal (flotante), una cadena de texto, realizando la asignación explicita y la asignación inferida
+*/
+
+/*:
+### Asociación de tipos
+A) Declara el tipo de dato por asociación para un tipo de dato String
+*/
+
+//: B) Declara el tipo de dato por asociación para un tipo de dato  Número entero.
+
+
+
+/*: 
+### Arreglos y Diccionarios
+A) Crea la variable "numeros" de tipo Array con números consecutivos del 1 a 10.
+*/
+
+//: B) Crea la variable "diasSemana" de tipo Dictionary con la relación numero:día Ej. 1:"Lunes"
+
+
+/*:
+### Condicionales y Ciclos
+A) Declarar la variable "datos" con los valores [3,6,9,2,4,1]
+*/
+
+//: B) realizar el recorrido de la variable "datos" con la instrucción "for"
+
+
+//: C) Encontrar los valores menores a 5
+
+
+
+
+
--- Actividad 3/MyPlayground.playground/Contents.swift
+++
@@ -1,5 +0,0 @@
-import UIKit
-
-let entero = 500
-let flotante = 5.5
-let text
