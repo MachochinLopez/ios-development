f4bcfa5703f9070cac1403ee45beb6b9f0dd3a47
Oscar López <machochinnio@hotmail.com>
2021-03-12 11:17:13 -0600

    Playground Actividad 6 starter

---
+++ Actividad 6/Actividad6_OALR.playground/Contents.swift
@@ -0,0 +1,48 @@
+import UIKit
+/*:
+# Playground - Actividad 6
+* Operadores personalizados
+* Subscripts
+* Control de errores
+
+*/
+
+
+/*: 
+### Operadores personalizados
+A) Crear el operador para realizar la potencia de el valor "a" a la potencia "b" en valores enteros
+*/
+
+
+
+
+//: B) Crear el operador |> para ordenar la colección [2,5,3,4] de menor a mayor
+
+
+
+/*:
+### Subscripts
+A) Del conjunto de datos en el Array [2,3,4,5], crear el subscript para modificar los valores multiplicados por el valor 2 y extraer al valor dado un índice.
+*/
+
+
+
+
+
+
+//: B) Crear el Struct para definir u obtener la posición  para los personaje de tipo Enemigo donde cada posición es de tipo CGPoint aplicnado subscritps
+
+
+/*:
+### Control de Errores
+A) Crear la función ExisteValor en la cual se reciba como parámetro el valor a buscar dentro de un colección ["A":1, "B":2,"C":3]
+*/
+
+
+
+
+
+
+
+
+
