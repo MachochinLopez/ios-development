c076a9fd567f957c5cc7e724e7268cff36ad96f1
Oscar López <machochinnio@hotmail.com>
2021-03-14 16:50:55 -0600

    Archivo inicial de la actividad

---
+++ Actividad 7/TM_A_07.playground/Contents.swift
@@ -0,0 +1,52 @@
+import UIKit
+/*:
+# Playground - Actividad 7
+* Valor por tipo y por referencia
+* Funciones personalizadas Y Genericos
+* Funciones de la biblioteca Swift para Arreglos
+*/
+
+
+/*: 
+### Valor por tipo y por referencia
+A) Para la colección "var costo_referencia:[Float] = [8.3,10.5,9.9]", aplicar el impuesto del 16% a través de recorrer la colección "costo_referencia" para aplicar el impuesto a cada cantidad, crear una función Impuesto que recibe como parámetro la colección y regrese aplicado el impuesto a cada cantidad.
+*/
+
+
+
+
+
+
+//: B) Crear la función "sumaTres"  que reciba una función con dos valores a sumar y un segundo parametro para sumar el tercer número.
+
+
+
+
+
+
+/*:
+### Funciones personalizadas y Genéricos
+
+ 
+ A) Generics: Crear la función genérica para intercambiar valores entre dos variables del mismo tipo.
+*/
+
+
+
+//: B) Función personalizada: Crear la función "Transformar" que reciba como parámetro una colección de tipo Int  "var datos = [3,7,9,2]" y una función que transforme cada valor de la coleción en una operación definida fuera de la función, regresando una colección transformada.
+
+
+
+/*:
+### Biblioteca de Swift
+A) Aplicar la función de librería de Swift "map" para la colección var precios = [4.2, 5.3, 8.2, 4.5] y aplicar el impuesto de 16% y asignarla a la variable "impuesto"
+*/
+
+
+
+//: B) Aplicar la función de la librería de Swift "filter" para la colección resultante "impuesto" del paso A, en donde se obtengas solo los precios mayores a 6.0 y asignarlos a la variable "precio_menor"
+
+
+
+
+
