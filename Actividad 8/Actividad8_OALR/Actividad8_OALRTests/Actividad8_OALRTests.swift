947a6dc8ee87f03fdd7fdf6e6e55adfcb38b70dc
Oscar Alejandro Lopez Ramos <machochinnio@hotmail.com>
2021-03-16 17:16:16 -0400

    Se anadio la imageview al main.storyboard

---
+++ Actividad 8/Actividad8_OALR/Actividad8_OALRTests/Actividad8_OALRTests.swift
@@ -0,0 +1,34 @@
+//
+//  Actividad8_OALRTests.swift
+//  Actividad8_OALRTests
+//
+//  Created by user183079 on 3/16/21.
+//  Copyright © 2021 user183079. All rights reserved.
+//
+
+import XCTest
+@testable import Actividad8_OALR
+
+class Actividad8_OALRTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
