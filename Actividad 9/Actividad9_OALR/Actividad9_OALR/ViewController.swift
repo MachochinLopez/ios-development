6f9e82d86e8616aa9f9c377e7998c246258f1d63
Oscar Alejandro Lopez Ramos <machochinnio@hotmail.com>
2021-03-16 20:16:30 -0400

    Actividad 9 resuelta

---
+++ Actividad 9/Actividad9_OALR/Actividad9_OALR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Actividad9_OALR
+//
+//  Created by user183079 on 3/16/21.
+//  Copyright © 2021 user183079. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
