1030a2e20d12e9ba58fc8b366d786c811d23f227
Oscar López <machochinnio@hotmail.com>
2021-05-22 00:00:27 -0500

    Archivo inicial Actividad 12

---
+++ Actividad 12/GestoAnimacion/GestoAnimacion/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  GestoAnimacion
+//
+//  Created by alicharlie on 11/05/16.
+//  Copyright © 2016 codepix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipoGesto: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+    }
+
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+
+    
+    }
+    
+    
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        
+    }
+
+    
+    
+    
+
+}
+
