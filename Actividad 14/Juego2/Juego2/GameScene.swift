4d9034bfb665e5a4f43e6bf52613e7b0ec2bb500
Oscar Alejandro Lopez Ramos <machochinnio@hotmail.com>
2021-05-22 12:33:39 -0400

    Actividad 14 terminada

--- Actividad 14/Juego2/Juego2/GameScene.swift
+++ Actividad 14/Juego2/Juego2/GameScene.swift
@@ -1,44 +1,28 @@
-//
-//  GameScene.swift
-//  Juego2
-//
-//  Created by alicharlie on 19/05/16.
-//  Copyright (c) 2016 codepix. All rights reserved.
-//
-
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        
-        self.backgroundColor = UIColor.darkGrayColor()
     
-      
+    override func didMove(to: SKView) {
+        
+        self.backgroundColor = UIColor.darkGray
+    
+        let fondo = SKSpriteNode(imageNamed: "fondo2")
+        fondo.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        fondo.zPosition = -1
+        self.addChild(fondo)
+        let num1 = SKSpriteNode(imageNamed: "1")
+        num1.position = CGPoint(x: self.frame.midX, y: self.frame.midY*1.2)
+        self.addChild(num1)
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       
-        
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
-            let location = touch.locationInNode(self)
+            let location = touch.location(in: self)
             print(location)
         }
     }
    
-    
-    
-    override func update(currentTime: CFTimeInterval) {
+    override func update(_ currentTime: CFTimeInterval) {
         
     }
-
 }
